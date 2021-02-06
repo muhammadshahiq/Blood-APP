29c86831c18185f50e010f11c9ec5fa4f069a9aa
Muhammadshahiq <Muhammadshahiq1999@gmail.com>
2021-02-06 21:14:39 +0500

    Blood App

---
+++ AwesomeProject/index.js
@@ -0,0 +1,16 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+import bgMessaging from './src/utilities/bgMessaging';
+
+AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => bgMessaging);
+
+
+
+
